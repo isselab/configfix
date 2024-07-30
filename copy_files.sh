659e95baa346a2a3e0ed8037c652d1f1f45204d0
Ole Schuerks <ole0811sch@gmail.com>
2024-07-30 17:44:19 +0200

    Remove scripts and copy-script

--- copy_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [[ $# -ne 1 ]]; then
-	echo "Illegal number of parameters."
-	echo "Usage: copy_files.sh \$KERNEL_SOURCE_TREE"
-	exit 1
-fi
-
-cp -r ./scripts/kconfig $1/scripts
-echo "Files copied successfully. Now run 'make xconfig' in $1"
